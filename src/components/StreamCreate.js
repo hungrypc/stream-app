379cc835b077e5a7f7e4706abe1cbab08eb7fd16
Phil Chan <philipchanhk626@gmail.com>
2020-01-14 20:21:53 +0000

    form fields for streamCreate complete

--- src/components/StreamCreate.js
+++ src/components/StreamCreate.js
@@ -2,21 +2,58 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends Component {
-    renderInput(formProps) {
-        return <input type="text" {...formProps.input} />
+    renderError({ error, touched }) {
+        if (touched && error) {
+            return(
+                <div className="ui error message">
+                    <div className="header">{error}</div>
+                </div>
+            )
+        }
+    }
+
+    renderInput = ({ input, label, meta }) => {
+        const className = `field ${meta.error && meta.touched ? 'error':''}`
+        return (
+            <div className={className}>
+                <label>{label}</label>
+                <input type="text" {...input} autoComplete="off" />
+                {this.renderError(meta)}
+            </div>
+        )
+    }
+
+    onSubmit(formValues) {
+        
+
     }
 
     render() {
         return (
-            <form>
-                <Field name="title" component={this.renderInput} />
-                <Field name="description" component={this.renderInput} />
+            <form className="ui form error" onSubmit={this.props.handleSubmit(this.onSubmit)}>
+                <Field name="title" component={this.renderInput} label="Enter Title" />
+                <Field name="description" component={this.renderInput} label="Enter Description" />
+                <button className="ui button primary">Submit</button>
             </form>
         )
 
     }
 };
 
+const validate = (formValues) => {
+    const errors = {};
+    if(!formValues.title) {
+        errors.title = 'Please enter a title';
+    }
+
+    if(!formValues.description) {
+        errors.description = 'Please enter a description';
+    }
+
+    return errors;
+};
+
 export default reduxForm({
-    form: 'streamCreate'
+    form: 'streamCreate',
+    validate
 })(StreamCreate);
